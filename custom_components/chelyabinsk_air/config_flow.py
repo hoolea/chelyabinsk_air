9192f675e70b7f75cfd3eedcb0d8ff41484c3e18
Hoolea Wonderlazer <100796653+hoolea@users.noreply.github.com>
2026-04-08 16:16:40 +0500

    Update config_flow.py

--- custom_components/chelyabinsk_air/config_flow.py
+++ custom_components/chelyabinsk_air/config_flow.py
@@ -18,6 +18,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
-                vol.Required("stations"): cv.multi_select({s: s for s in STATIONS})
+                vol.Required("stations"): cv.multi_select(STATIONS)
             })
         )
